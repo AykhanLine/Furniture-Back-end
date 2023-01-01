09a009da15efa40c1cb0af934cb200eb09e8ef3c
AykhanLine <ayxankerimli2001@mail.ru>
2023-01-01 14:42:59 +0400

    Add project files.

---
+++ Furniture Ecommerce Web  Back end/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Furniture_Ecommerce_Web__Back_end.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
